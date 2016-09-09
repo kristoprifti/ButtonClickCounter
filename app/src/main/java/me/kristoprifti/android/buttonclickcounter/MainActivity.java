89ef2a2ab97d7bef5c5d25863d9038d3607d3edb
Kristo Prifti <kristopriti93@gmail.com>
2016-09-09 18:06:31 +0300

    Initial commit

---
+++ app/src/main/java/me/kristoprifti/android/buttonclickcounter/MainActivity.java
@@ -0,0 +1,40 @@
+package me.kristoprifti.android.buttonclickcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        userInput.setText("");
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = userInput.getText().toString();
+                result = result + "\n";
+                textView.append(result);
+                userInput.setText("");
+            }
+        };
+
+        if (button != null)
+            button.setOnClickListener(ourOnClickListener);
+    }
+}
